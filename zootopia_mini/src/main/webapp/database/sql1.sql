e4381860d897cc506d0906925f28ba28e44bf8b0
joha <163955181+jeongeunsun@users.noreply.github.com>
2024-05-31 18:14:48 +0900

    회원정보수정 수정
    종류, 성별 수정

--- zootopia_mini/src/main/webapp/database/sql1.sql
+++ zootopia_mini/src/main/webapp/database/sql1.sql
@@ -1,36 +1,28 @@
-INSERT INTO member (nickname, userid, pwd, name, phone, email, petname, petgender, kind) VALUES
-('역학계몽요해', 'user1', '1111', '홍길동', '010-1234-5678', 'user1@example.com', 'ALEX', 'F' ,'강아지'),
-('서바두턔바쨰', 'user2', '2222', '김철수', '010-9876-5432', 'user2@example.com', '카미', 'F', '고양이'),
-('서매빠디뿌', 'user3', '3333', '이영희', '010-1111-2222', 'user3@example.com', NULL, NULL, NULL),
-('퐤니모', 'user4', '4444', '박미영', '010-3333-4444', 'user4@example.com', NULL, NULL, NULL),
-('쭤쥬뢰포', 'user5', '5555', '정영자', '010-5555-6666', 'user5@example.com', '초코', 'F', '강아지'),
-('마주잡기', 'user6', '6666', '이성민', '010-7777-8888', 'user6@example.com', NULL, NULL, NULL),
-('SunnySmile', 'user7', '7777', '최영수', '010-9999-0000', 'user7@example.com', '뽀삐', 'F', '강아지'),
-('일난풍화한', 'user8', '8888', '김미희', '010-1234-5678', 'user8@example.com', '로미', 'F', '고양이'),
-('LunaBeam', 'user9', '9999', '박철호', '010-9876-5432', 'user9@example.com', '맥스', 'M', '강아지'),
-('WiseOwl', 'user10', '10101010', '이미숙', '010-1111-2222', 'user10@example.com', '마이클', 'M', '고양이');
-
 
 select * from member;
 
 
-INSERT INTO community (vcount, userid, nickname, subject, content, recommands, kind, createdate)				
-VALUES				
-(0, 'user1', '역학계몽요해', '첫 번째 글', '모든 국민은 언론·출판의 자유와 집회·결사의 자유를 가진다. 대통령은 제4항과 제5항의 규정에 의하여 확정된 법률을 지체없이 공포하여야 한다. 제5항에 의하여 법률이 확정된 후 또는 제4항에 의한 확정법률이 정부에 이송된 후 5일 이내에 대통령이 공포하지 아니할 때에는 국회의장이 이를 공포한다.				
-국방상 또는 국민경제상 긴절한 필요로 인하여 법률이 정하는 경우를 제외하고는, 사영기업을 국유 또는 공유로 이전하거나 그 경영을 통제 또는 관리할 수 없다.', 0, 1, NOW()),				
-(0, 'user2', '서바두턔바쨰', '두 번째 글', '헌법개정은 국회재적의원 과반수 또는 대통령의 발의로 제안된다. 모든 국민은 통신의 비밀을 침해받지 아니한다.				
-행정권은 대통령을 수반으로 하는 정부에 속한다.', 0, 2, NOW()),				
-(0, 'user3', '서매빠디뿌', '세 번째 글', '국민경제의 발전을 위한 중요정책의 수립에 관하여 대통령의 자문에 응하기 위하여 국민경제자문회의를 둘 수 있다.				
-국가원로자문회의의 조직·직무범위 기타 필요한 사항은 법률로 정한다.', 0, 0, NOW()),				
-(0, 'user4', '퐤니모', '네 번째 글', '모든 국민은 자기의 행위가 아닌 친족의 행위로 인하여 불이익한 처우를 받지 아니한다.				
-사회적 특수계급의 제도는 인정되지 아니하며, 어떠한 형태로도 이를 창설할 수 없다.', 0, 0, NOW()),				
-(0, 'user5', '쭤쥬뢰포', '다섯 번째 글', '모든 국민은 통신의 비밀을 침해받지 아니한다.				
-국토와 자원은 국가의 보호를 받으며, 국가는 그 균형있는 개발과 이용을 위하여 필요한 계획을 수립한다.', 0, 1, NOW()),				
-(0, 'user6', '마주잡기', '여섯 번째 글', '여섯 번째 글 내용입니다.', 0, 0, NOW()),				
-(0, 'user7', 'SunnySmile', '일곱 번째 글', '일곱 번째 글 내용입니다.', 0, 1, NOW()),				
-(0, 'user8', '일난풍화한', '여덟 번째 글', '여덟 번째 글 내용입니다.', 0, 2, NOW()),				
-(0, 'user9', 'LunaBeam', '아홉 번째 글', '아홉 번째 글 내용입니다.', 0, 1, NOW()),				
-(0, 'user10', 'WiseOwl', '열 번째 글', '열 번째 글 내용입니다.', 0, 2, NOW());				
+INSERT INTO member (nickname, userid, pwd, name, phone, email, petname, petgender, kind) VALUES
+('닉네임1', 'user1', '0000', '홍길동', '010-1234-5678', 'user1@example.com', 'ALEX', 'F' ,'강아지'),
+('닉네임2', 'user2', '0000', '김철수', '010-9876-5432', 'user2@example.com', '카미', 'F', '고양이'),
+('닉네임3', 'user3', '0000', '이영희', '010-1111-2222', 'user3@example.com', '뽀삐', 'F', '강아지'),
+('닉네임4', 'user4', '0000', '박미영', '010-3333-4444', 'user4@example.com', '맥스', 'M', '강아지');
 
 
-select * from community;
+INSERT INTO member (nickname, userid, pwd, name, phone, email, petname, petgender, kind , image , saveimage) VALUES
+('닉네임5', 'user5', '0000', '홍길동', '010-1234-5678', 'user1@example.com', 'ALEX', 'F' ,'강아지' , 'profileimage1.jpg' , 'profileimage1.jpg');
+
+
+INSERT INTO contest (userid, useyn, subject, content, lastdate)
+values ('user1','Y','누가 더 사랑스러운 반려동물을 가지고 있는가!' , '사랑스러운 반려동물들을 찾아보세요!! 지금 바로 여러분의 귀여운 동물 사진들을 올려주세요!' ,
+DATE_ADD(NOW(), INTERVAL 3 DAY));
+
+insert into contest_pet (userid  , cseq , content , image, saveimage ) values 
+('user2', 1 ,'저희 강아지좀 보세요 대박이에요.2' , 'pet_image2.jpg','pet_image2.jpg' );
+insert into contest_pet (userid  , cseq , content , image, saveimage) values 
+('user1', 1 ,'저희 강아지좀 보세요 대박이에요.' , 'pet_image1.jpg','pet_image1.jpg' );
+
+insert into contest_pet (userid  , cseq , content , image, saveimage) values 
+('user2', 1 ,'저희 강아지좀 보세요 대박이에요.2' , 'pet_image2.jpg','pet_image2.jpg' );
+insert into contest_pet (userid  , cseq , content , image, saveimage) values 
+('user1', 1 ,'저희 강아지좀 보세요 대박이에요.' , 'pet_image1.jpg','pet_image1.jpg' );
